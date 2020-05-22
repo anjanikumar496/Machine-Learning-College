6ba6f12f1508c44e7fc4d9def3fe8b9212871d18
Anjani kumar <43503509+anjanikumar496@users.noreply.github.com>
2020-05-22 09:35:22 +0530

    Delete support-vector-Classifier.py

--- support-vector-Classifier.py
+++
@@ -1,9 +0,0 @@
-from import_package import  DataPreprocessin
-
-imported_data = DataPreprocessin.Process()
-
-from  sklearn.svm import SVC
-svc = SVC()
-
-print(svc.fit(imported_data.get('X_train'),imported_data.get('y_train')))
-
